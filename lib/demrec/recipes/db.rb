710ed004b696eb96ffc7bc57ca30527bae034882
Mike Bailey <mike@bailey.net.au>
2011-07-28 16:35:48 +1000

    demrec-0.0.6

--- lib/demrec/recipes/db.rb
+++ lib/demrec/recipes/db.rb
@@ -1,10 +1,12 @@
-namespace :demrec do
-  namespace :db do
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :demrec do
+    namespace :db do
 
-    desc "Backup the database"
-    task :backup do
-      puts "Capistrano is backing up db"
+      desc "Backup the database"
+      task :backup do
+        puts "Capistrano is backing up db"
+      end
+
     end
-
   end
 end
